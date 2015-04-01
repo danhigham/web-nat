652b729173fe6a6f20cbcc33da6827907e097951
root <root@e3a7288d4a00.(none)>
2015-04-01 13:36:54 +0000

    Initial version

---
+++ iptables.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type IPTable struct {
+	Chains	[]IPTableChain
+}
+
+type IPTableChain struct {
+	Rows	[]IPTableRow
+}
+
+type IPTableRow struct {
+	DestIP 		string
+	DestPort	int
+	SrcPort		int
+}
+
+func (table IPTable) Load(tableName string) {
+	
+}
+
+func main() {
+
+}
+
